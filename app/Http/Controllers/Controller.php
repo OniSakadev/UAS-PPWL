0ce3d4ab8896f5ecc87302bf81b99d99b0179362
Muhammad Ilham Zulfaishal <77421455+OniSakadev@users.noreply.github.com>
2025-05-25 21:47:53 +0700

    fixing on issue PSR-4 Autoloading Issue

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,20 +1,8 @@
 <?php
- 
+
 namespace App\Http\Controllers;
- 
-use App\Models\User;
-use Inertia\Inertia;
-use Inertia\Response;
- 
-class UserController extends Controller
+
+abstract class Controller
 {
-    /**
-     * Show the profile for a given user.
-     */
-    public function show(string $id): Response
-    {
-        return Inertia::render('users/show', [
-            'user' => User::findOrFail($id)
-        ]);
-    }
+    //
 }
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use App\Models\User;
+use Inertia\Inertia;
+use Inertia\Response;
+ 
+class UserController
+{
+    /**
+     * Show the profile for a given user.
+     */
+    public function show(string $id): Response
+    {
+        return Inertia::render('users/show', [
+            'user' => User::findOrFail($id)
+        ]);
+    }
+}
